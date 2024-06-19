7ebe9a6d32f5a91c5ad495f0fb6bdee224f26769
Carlospazsoy <carlosra.hz@gmail.com>
2024-06-18 19:08:04 -0600

    baseURL https://p5-backend-9n4z.onrender.com

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,9 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: 'https://api-auth-3uh2.onrender.com'
-  baseURL: 'http://localhost:4000'
+  // baseURL: 'http://localhost:4000'
+  baseURL: 'https://p5-backend-9n4z.onrender.com'
+
 })
 
 // Muestra la URL base en la consola
